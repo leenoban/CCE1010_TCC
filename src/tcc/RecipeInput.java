4cc59a8bc256df12daec7e359f48c0bae8895427
Joe <Joe@192.168.11.242>
2015-03-05 16:00:45 +0800

    GUI and modify list display

--- src/tcc/RecipeInput.java
+++ src/tcc/RecipeInput.java
@@ -9,6 +9,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JDialog;
 import to.Recipe;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;  
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JFrame; 
+import javax.swing.JTabbedPane; 
+import javax.swing.JLabel; 
+import javax.swing.JPanel; 
+import javax.swing.JButton; 
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,6 +35,43 @@ public class RecipeInput extends JDialog implements ActionListener {
     
     private TCC parent;
     private String mode;
+    // Joe new added
+    JPanel panelNew = new JPanel();
+    JPanel panelButton = new JPanel();
+    JPanel panelLeft = new JPanel(); 
+    JPanel panelRight = new JPanel(); 
+    JPanel panelRadioButton = new JPanel();
+    JPanel panelOne = new JPanel();
+    JPanel panelTop1= new JPanel();
+    JPanel panelTwo = new JPanel();
+    JPanel panelTop2= new JPanel(); 
+    JPanel panelThree = new JPanel();
+    JTabbedPane tbd1 = new JTabbedPane(); 
+    JButton btnCancel = new JButton("Cancel");
+    JButton btnSave = new JButton("Save");
+    JLabel lblRecipeName = new JLabel("Recipe Name");
+    JTextField txtRecipeName = new JTextField(8);
+    JLabel lblCountry = new JLabel("Country");
+    JComboBox cboCountry = new JComboBox();
+    JLabel lblCookingMethod = new JLabel("Cooking Method");
+    JComboBox cboCookingMethod = new JComboBox();
+    JLabel lblTimeRequire = new JLabel("Time Require");
+    JComboBox cboTimeRequire = new JComboBox();
+    JLabel lblDifficulties = new JLabel("Difficulties");
+    JRadioButton radio5 = new JRadioButton("\t5");
+    JRadioButton radio4 = new JRadioButton("\t4");
+    JRadioButton radio3 = new JRadioButton("\t3");
+    JRadioButton radio2 = new JRadioButton("\t2");
+    JRadioButton radio1 = new JRadioButton("\t1");
+    ButtonGroup radioButtonGroup = new ButtonGroup();
+    JTextArea txtMaterial = new JTextArea();
+    JTextArea txtStep = new JTextArea();
+    JScrollPane scrollMaterial = new JScrollPane(txtMaterial,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    JScrollPane scrollStep = new JScrollPane(txtStep,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    
+    //Joe new add
     
     public RecipeInput(TCC m_parent) {
         this.parent = m_parent;
@@ -34,16 +87,123 @@ public class RecipeInput extends JDialog implements ActionListener {
     }
     
     private void buildGUI() {
-        // TODO: Joe - build GUI
+        // TODO: Joe - buildGUI
+   
+        
+        // Joe new add
+        //panel to hold tabs and button
+            panelButton.setLayout(new GridLayout(1,2));
+            panelButton.add(btnCancel);
+            panelButton.add(btnSave);
+            btnCancel.addActionListener(this);
+            btnSave.addActionListener(this);
+            //Tab 1 - top panel
+
+            panelLeft.setLayout(new GridLayout(5,0));
+            panelLeft.add(lblRecipeName);
+            panelLeft.add(lblCountry);
+            panelLeft.add(lblCookingMethod);
+            panelLeft.add(lblTimeRequire);
+            panelLeft.add(lblDifficulties);
+            
+            panelRight.setLayout(new GridLayout(5,0));
+            panelRight.add(txtRecipeName);
+            panelRight.add(cboCountry);
+            panelRight.add(cboCookingMethod);
+            panelRight.add(cboTimeRequire);
+            
+            //Tab 1 - panel to hold radio button with different layout  
+            
+            panelRadioButton.setLayout(new FlowLayout(FlowLayout.LEFT));  
+            radioButtonGroup.add(radio5);
+            radioButtonGroup.add(radio4);
+            radioButtonGroup.add(radio3);
+            radioButtonGroup.add(radio2);
+            radioButtonGroup.add(radio1);
+            panelRadioButton.add(radio5);
+            panelRadioButton.add(radio4);
+            panelRadioButton.add(radio3);
+            panelRadioButton.add(radio2);
+            panelRadioButton.add(radio1);
+            
+            //create a layout to hold the label,combox,textfield and radio button panel for Tab1
+            panelOne.setLayout(new GridLayout(1,1));
+            panelOne.add(panelLeft);
+            panelOne.add(panelRight);
+            panelRight.add(panelRadioButton);
+            
+            //Tab 2 - create text area
+            panelTop1.setLayout(new GridLayout(1,0));
+            txtMaterial.setLineWrap(true);
+            txtMaterial.setWrapStyleWord(true);
+            panelTop1.add(scrollMaterial);
+            
+            //create a layout to hold the text area for tab2
+            panelTwo.setLayout(new BorderLayout(0,0));
+            panelTwo.add(panelTop1, BorderLayout.CENTER);
+            panelTwo.add(scrollMaterial);
+            
+            //Tab3 - create text area          
+            panelTop2.setLayout(new BorderLayout(1,0));
+            txtStep.setLineWrap(true);
+            txtStep.setWrapStyleWord(true);
+            panelTop2.add(scrollStep);
+            
+            //create a layout to hold the text area for tab2
+            panelThree.setLayout(new BorderLayout(5,10));
+            panelThree.add(panelTop2, BorderLayout.CENTER);
+             
+            //Create tabs
+            tbd1.addTab("Basic Information", panelOne);
+            tbd1.addTab("Material", panelTwo);
+            tbd1.addTab("Steps", panelThree);
+            
+            //Panel to hold the tabs and button
+            add(tbd1,BorderLayout.NORTH);
+            add(panelButton,BorderLayout.SOUTH);
+        //Joe new add    
+            
         this.setModal(true);
         this.setTitle(Constants.TITLE_RECIPE + " - " + this.mode);
         this.setSize(800, 600);
-        //pack();
+        pack();
     }
     
     // fill data to the recipe form from Recipe object (for update purpose)
     private void fillData(Recipe m_recipe) {
         // TODO: Joe - fill data
+        //Joe new add
+        int chkRecipeID = m_recipe.getRecipe_id();
+        int chkLevel=Recipe.getRecipe(chkRecipeID).getLevel();
+        txtRecipeName.setText(Recipe.getRecipe(chkRecipeID).getRecipe_name());
+        cboCountry.addItem(Recipe.getRecipe(chkRecipeID).getCountry().getCountry_name());
+        cboCookingMethod.addItem(Recipe.getRecipe(chkRecipeID).getMethod().getMethod_name());
+        cboTimeRequire.addItem(Recipe.getRecipe(chkRecipeID).getInterval().getInterval());
+        
+        switch (chkLevel)
+        {
+            case 5:
+                radio5.setSelected(true);
+                break;
+            case 4:
+                radio4.setSelected(true);
+                break;    
+            case 3:
+                radio3.setSelected(true);
+                break;
+            case 2:
+                radio2.setSelected(true);
+                break;
+            case 1:
+                radio1.setSelected(true);
+                break;
+            default:
+                break;
+        }
+        
+        txtMaterial.setText(Recipe.getRecipe(chkRecipeID).getMaterial());
+        txtStep.setText(Recipe.getRecipe(chkRecipeID).getSteps());
+        //Joe new add
     }
     
     // Update DB record - for save button call
@@ -66,11 +226,25 @@ public class RecipeInput extends JDialog implements ActionListener {
         ri.setLocationRelativeTo(null);
         ri.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         ri.setVisible(true);
+        
     }
-
+   
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        ;
-    }
-    
+        
+        // joe new add
+        Object obj = e.getSource();
+            if(obj==btnCancel) {
+        
+                cancel();
+            }
+            else if (obj==btnSave)
+                {
+                //updateRecipe(m_recipe);
+                JOptionPane.showMessageDialog(null,"Saved");
+                }
+        
+        }
+        //Joe new add
 }
